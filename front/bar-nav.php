a28cf390666169cde92f05f3eb87c5565e8063ee
FearZYTB <95861079+FearZYTB@users.noreply.github.com>
2021-12-09 15:59:34 +0100

    Front (Home, Page Champion, Connexion, Panier)

---
+++ front/bar-nav.php
@@ -0,0 +1,11 @@
+
+<html>
+
+<ul class="nav nav-pills">
+    <li class="nav-item"><a href="index.php" class="nav-link active" aria-current="page">Menu</a></li>
+    <li class="nav-item"><a href="page-acheter-un-champion.php" class="nav-link">Acheter un NFT Champion</a></li>
+    <li class="nav-item"><a href="connexion.php" class="nav-link">Connexion</a></li>
+    <li class="nav-item"><a href="panier.php" class="nav-link">Panier</a></li>
+</ul>
+
+</html>
